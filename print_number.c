38c6be6fac00d8c78b846f441299e3b377c49d18
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-05-24 23:09:32 +0100

    betty check

--- print_number.c
+++ print_number.c
@@ -1,12 +1,16 @@
 #include "shell.h"
-
+/**
+ * print_int - Prints an integer to the standard output.
+ * @n: The integer to print.
+ */
 void print_int(int n)
 {
-	if (n < 0) {
+	if (n < 0)
+	{
 		putchar('-');
 		n = -n;
 	}
-	if (n/10)
+	if (n / 10)
 		print_int(n / 10);
 	print_char((n % 10) + '0');
 }
