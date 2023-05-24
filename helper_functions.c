9cbcf94a831e1002f5795e78c122eb7fb0d9cdc1
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-05-24 22:51:57 +0100

    betty check

--- helper_functions.c
+++ helper_functions.c
@@ -1,5 +1,10 @@
 #include "shell.h"
-
+/**
+ * str_len - Computes the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string.
+ */
 int str_len(char *str)
 {
 	int i = 0;
@@ -10,7 +15,10 @@ int str_len(char *str)
 		i++;
 	return (i);
 }
-
+/**
+ * free_array - Frees memory allocated for an array of strings.
+ * @arr: The array of strings to free.
+ */
 void free_array(char **arr)
 {
 	int index;
@@ -19,7 +27,11 @@ void free_array(char **arr)
 		free(arr[index]);
 	free(arr);
 }
-
+/**
+ * free_f - Frees memory allocated for multiple variables.
+ * @n: The number of variables.
+ * @...: Variable number of arguments to be freed.
+ */
 void free_f(int n, ...)
 {
 	int i;
@@ -29,15 +41,19 @@ void free_f(int n, ...)
 	va_start(a_list, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(a_list, char*);
+		str = va_arg(a_list, char *);
 		if (str == NULL)
 			str = "(nil)";
 		free(str);
 	}
 	va_end(a_list);
 }
-
-
+/**
+ * printing_error - Prints an error message to the standard error output.
+ * @av: The name of the program.
+ * @count: The line number where the error occurred.
+ * @command: The command causing the error.
+ */
 void printing_error(char *av, int count, char *command)
 {
 	print_string(av, 1);
@@ -46,8 +62,12 @@ void printing_error(char *av, int count, char *command)
 	print_string(": ", 1);
 	print_string(command, 1);
 }
-
-
+/**
+ * error_execute - Prints an error message and exits the program.
+ * @av: The name of the program.
+ * @count: The line number where the error occurred.
+ * @tmp_command: The command causing the error.
+ */
 void error_execute(char *av, int count, char *tmp_command)
 {
 	printing_error(av, count, tmp_command);
